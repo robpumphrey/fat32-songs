ac6604ee76abbdbcfbbb2c0c273c647350d66b90
Rob Pumphrey <rob.pumphrey@certus-tech.com>
2018-03-17 20:14:14 +0000

    Update script to work with multiple dirs

--- src/TrackRelabel.sh
+++ src/TrackRelabel.sh
@@ -9,7 +9,7 @@ cat <<'__end_of_usage__'
 ## Move mp3 files into one master folder
 ## touch playlist.m3u in master
 ## cat each playlist.m3u into the master
-## run TrackRelabel.sh -A "Ablum Name" -a "Artist Name" -y year
+## run TrackRelabel.sh -A "Ablum Name" -a "Artist Name" -y year -p <directory pattern>
 ####################################################################
 __end_of_usage__
 exit 1
@@ -19,12 +19,14 @@ artist=
 album=
 year=1970
 genre=101
+pattern='*'
 
-while getopts ":a:A:y:H" argname; do
+while getopts "p:a:A:y:H" argname; do
  case $argname in
     A) album=$OPTARG ;;
     a) artist=$OPTARG ;;
     y) year=$OPTARG ;;
+    p) pattern=$OPTARG ;;
     H ) usage
        exit 0;;
     : ) error "Option '$OPTARG' requires an argument.";;
@@ -45,22 +47,31 @@ if [ ! -e playlist.m3u ]; then
   echo "Must have a playlist.m3u file"
   exit 1
 fi
+echo "Diretories to use are "
+ls -1d $pattern
+echo ""
+echo "Carry on? (Enter for y, CTRL-C to abort)"
+read carryon
+
 
 playlistname=`echo "${artist}-${album}" | sed -e 's/ //g'`
 trackname=`echo "${album}" | sed -e 's/ //g'`
 track=1
-echo -n "" > "${playlistname}.m3u"
+targetdir="../${artist}-${album}/"
+mkdir "${targetdir}"
+
+fullplaylist="${targetdir}/${playlistname}.m3u"
+echo -n "" > "${fullplaylist}"
 IFS='
 '
-for f in `cat playlist.m3u`; do
-  printf -v g "%s-%02d.mp3" $trackname $track
-  echo "$g" >> ${playlistname}.m3u
-  if [ "$g" != "$f" ]; then
-    echo "$f moving to $g"
-    mv "$f" "$g"
-  fi
-  printf -v chapter "%02d" $track
-  id3 -t "Chapter $track" -T $track -a "${artist}" -A "${album}" -y "${year}" -g "${genre}" "$g"
-  let track=$track+1
+for d in $pattern; do
+  for f in `cat "$d/playlist.m3u"`; do
+    printf -v g "%s-%02d.mp3" $trackname $track
+    echo "$g" >> ${fullplaylist}
+    echo "copying $f to ${targetdir}$g"
+    cp "$f" "${targetdir}$g"
+    printf -v chapter "%02d" $track
+    id3 -t "Chapter $track" -T $track -a "${artist}" -A "${album}" -y "${year}" -g "${genre}" "${targetdir}$g"
+    let track=$track+1
+  done
 done
-rm playlist.m3u
